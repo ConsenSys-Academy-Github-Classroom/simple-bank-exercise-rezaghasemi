7115e2dbf6000cf061453dbe1b3e5169fbc05d26
Reza Ghasemi <rezaghasemi.cs@gmail.com>
2021-10-12 19:04:41 +0330

    first steps

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -10,7 +10,7 @@ contract SimpleBank {
 
     /* State variables
      */
-    
+ 
     
     // Fill in the visibility keyword. 
     // Hint: We want to protect our users balance from other contracts
@@ -19,11 +19,11 @@ contract SimpleBank {
     // Fill in the visibility keyword
     // Hint: We want to create a getter function and allow contracts to be able
     //       to see if a user is enrolled.
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     // Let's make sure everyone knows who owns the bank, yes, fill in the
     // appropriate visilibility keyword
-    address owner = msg.sender;
+    address public owner = msg.sender;
     
     /* Events - publicize actions to external listeners
      */
@@ -62,6 +62,11 @@ contract SimpleBank {
     /// @return The users enrolled status
     // Emit the appropriate event
     function enroll() public returns (bool){
+      enrolled[msg.sender]=true;
+      emit LogEnrolled(msg.sender);
+      return enrolled[msg.sender];
+
+
       // 1. enroll of the sender of this transaction
     }
 
